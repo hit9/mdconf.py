2797565a1371371436656fe2fc14fa6bf1ddac27
hit9 <nz2324@126.com>
2013-06-07 16:05:09 +0800

    opmize the codes

--- mdconf.py
+++ mdconf.py
@@ -1,3 +1,11 @@
+# coding=utf8
+# Python implementation for visionmedia's mdconf,
+# the markdown driven configuration::
+#   https://github.com/visionmedia/mdconf
+# This module was built on the top of misaka:
+#   https://github.com/FSX/misaka
+
+
 import misaka
 from misaka import HtmlRenderer
 
@@ -5,57 +13,75 @@ from misaka import HtmlRenderer
 class MdconfRenderer(HtmlRenderer):
 
     def __init__(self, *args, **kwargs):
-        super(MdconfRenderer, self).__init__(self, *args, **kwargs)
-        self.reset()
+        super(MdconfRenderer, self).__init__(*args, **kwargs)
+        self.reset_vars()
 
-    def reset(self):
-        self.conf = {}
-        self.keys = []
+    def reset_vars(self):
+        """reset conf retult"""
+        self.conf = {}  # the return var
+        self.keys = []  # record current keys
 
     def normalize_text(func):
+        """decorator to strip the second parameter of func"""
         def wrapper(self, text, *args, **kwargs):
             text = text.strip()
             return func(self, text, *args, **kwargs)
         return wrapper
 
     def slide_to(self, keys):
-        """slide to current section's dict"""
+        """slide to given keys depth dict"""
         conf = self.conf
-        for key in self.keys:
+        for key in keys:
             conf = conf.setdefault(key, {})
         return conf
 
     @normalize_text
     def header(self, text, level):
-
+        # polish keys to given depth
         while len(self.keys) >= level:
             self.keys.pop()
         self.keys.append(text)
+        # check keys
         self.slide_to(self.keys)
 
     @normalize_text
     def list_item(self, text, is_ordered):
-        index = text.find(":")
-        key, value = text[:index].strip(), text[index+1:].strip()
-        conf = self.slide_to(self.keys)
-        conf[key] = value
+        index = text.find(":")  # try to find the first ':'
+        # found, maps
+        if index >= 0:
+            key, value = (
+                text[:index].strip(),
+                text[:index+1].strip(),
+            )
+            conf = self.slide_to(self.keys)
+            conf[key] = value
+        else:  # not found, list
+            pass
 
-renderer = MdconfRenderer()
 
-extensions = (
-    misaka.EXT_FENCED_CODE |
-    misaka.EXT_NO_INTRA_EMPHASIS |
-    misaka.EXT_AUTOLINK
-)
+class MdconfParser(object):
+
+    def __init__(self):
+        extensions = (
+            misaka.EXT_FENCED_CODE |
+            misaka.EXT_NO_INTRA_EMPHASIS |
+            misaka.EXT_AUTOLINK
+        )
+        self.renderer = MdconfRenderer()
+        self.markdown = misaka.Markdown(self.renderer, extensions=extensions)
+
+    def parse(self, text):
+        self.renderer.reset_vars()
+        self.markdown.render(text)
+        return self.renderer.conf
 
 
-md = misaka.Markdown(renderer, extensions=extensions)
+parser = MdconfParser()  # build a quick use parser
 
 
 def parse(text):
-    renderer.reset()
-    md.render(text)
-    return renderer.conf
+    return parser.parse(text)
+
 
 ## current tests
 
